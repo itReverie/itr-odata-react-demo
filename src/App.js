1c1899c1e921138aa03a86404fa156cbfd34cbcb
itreverie <brenda.jimenez@itreverie.com>
2018-01-17 15:51:35 -0500

    retireving data from an odata service

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OData from 'react-odata';
+
+const baseUrl = 'http://services.odata.org/V4/TripPinService/People';
+const query = { filter: { FirstName: 'Russell' } };
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
@@ -13,9 +19,35 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+       <OData baseUrl={baseUrl} query={query}>
+       {({ loading, error, data }) => (
+
+          <div>
+      {loading && <span>Loading... (()=>{console.log(loading)}) </span>}
+            {error && {/* handle error here */ }}
+            {data && data.value.map((d, i) => <div key={i} id={i}>{d.FirstName} {d.Gender}</div>)}
+            </div>
+        )}
+        </OData>
+
+
+
       </div>
     );
   }
+
+
+/* Setup consistent fetch responses */
+componentWillMount() {
+    fetch('http://services.odata.org/V4/TripPinService/People')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            return responseJson.value[0].FirstName
+        })
+        .catch((error) => { console.error(error) });
+
+}
 }
 
 export default App;
